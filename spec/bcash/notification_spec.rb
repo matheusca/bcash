41e3d1aa625522e58373fb37a05a905240bc971d
Matheus Caceres <macsilva@sp.r7.com>
2014-05-12 14:12:05 -0300

    update: rspec 3.0

--- spec/bcash/notification_spec.rb
+++ spec/bcash/notification_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Bcash::Notification do
 
   # Don't have all params. If you see all, visit: https://www.bcash.com.br/desenvolvedores/integracao-retorno-automatico-loja-online.html
-  def params
+  let(:params) do
     {
       id_transacao: '123',
       data_transacao: '20/04/2013',
@@ -11,13 +11,13 @@ describe Bcash::Notification do
       valor_original: '12.00',
     }
   end
+  subject{ Bcash::Notification.new(params) }
 
-  it "should return instance" do
-    notification = Bcash::Notification.new(params)
-    notification.id_transacao.should == '123'
-    notification.data_transacao.should == '20/04/2013'
-    notification.data_credito.should == '22/04/2013'
-    notification.valor_original.should == '12.00'
+  it "returns instance" do
+    expect(subject.id_transacao).to eq('123')
+    expect(subject.data_transacao).to eq('20/04/2013')
+    expect(subject.data_credito).to eq('22/04/2013')
+    expect(subject.valor_original).to eq('12.00')
   end
 
 end
